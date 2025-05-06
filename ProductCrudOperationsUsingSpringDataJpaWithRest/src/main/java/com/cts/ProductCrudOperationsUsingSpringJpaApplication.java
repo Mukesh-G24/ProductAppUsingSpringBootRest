6eef1845ab704f0d202799e936e2744459b9fb24
Mukesh_G <g.mukesh2409@gmail.com>
2025-05-06 11:17:06 +0530

    commited

--- ProductCrudOperationsUsingSpringDataJpaWithRest/src/main/java/com/cts/ProductCrudOperationsUsingSpringJpaApplication.java
+++ ProductCrudOperationsUsingSpringDataJpaWithRest/src/main/java/com/cts/ProductCrudOperationsUsingSpringJpaApplication.java
@@ -17,7 +17,6 @@ public class ProductCrudOperationsUsingSpringJpaApplication {
 	public static void main(String[] args) {
 		
 		SpringApplication.run(ProductCrudOperationsUsingSpringJpaApplication.class, args);
-		System.out.println("day1 changes...");
 		
 	}
 
